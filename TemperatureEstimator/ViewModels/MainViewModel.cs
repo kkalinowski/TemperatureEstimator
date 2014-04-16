90a01c542658b1c63e9b6e86bbf8b870b3e01b7f
Krzysztof Kalinowski <kkalinowski@outlook.com>
2014-04-16 16:10:04 +0200

    Show todays temperature on GUI

--- TemperatureEstimator/ViewModels/MainViewModel.cs
+++ TemperatureEstimator/ViewModels/MainViewModel.cs
@@ -12,6 +12,7 @@ namespace TemperatureEstimator.ViewModels
     [Singleton]
     public class MainViewModel : NotifyingObject
     {
+        #region Properties
         private List<DateTemperature> data;
         public List<DateTemperature> Data
         {
@@ -23,10 +24,23 @@ namespace TemperatureEstimator.ViewModels
             }
         }
 
+        private double todaysTemperature;
+        public double TodaysTemperature
+        {
+            get { return todaysTemperature; }
+            set
+            {
+                todaysTemperature = value;
+                OnPropertyChanged("TodaysTemperature");
+            }
+        }
+        #endregion
+
         public MainViewModel(DataManager dataManager)
         {
             dataManager.Load(Settings.Default.Airport);
             Data = dataManager.Data.Where(x => x.Date >= DateTime.Today.AddMonths(-2)).ToList();
+            TodaysTemperature = Data.Last().Temperature;
         }
     }
 }
